37d2570212486190aff8702315c54e68df0c5ad2
Josh Tell <jtell1997@gmail.com>
2026-01-08 09:37:11 +0800

    Fix configuration issues and create races controller

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
-import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
+import { defineConfig } from "vite"
+import RubyPlugin from "vite-plugin-ruby"
+import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-  plugins: [
-    RubyPlugin(),
-  ],
-  root: "frontend",
+  plugins: [RubyPlugin(), react()],
 })
